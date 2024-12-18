c4b61d00fc9c31ef2d97c8840d6cf0c1c9883fa1
Aldo <aldomapler@gmail.com>
2024-12-17 19:03:56 -0600

    fix: Cambiamos el clickEvent por un submit y limpiamos el input al agregar una tarea

--- 00-DOM/app.js
+++ 00-DOM/app.js
@@ -1,23 +1,30 @@
 const addButton = document.querySelector('#add-button')
 const todoInput = document.querySelector('#todo-input')
 const todosContainer = document.querySelector('#todo-container')
+const form = document.querySelector('#create-to-do-form')
 
 // Variable global para guardar los ToDo's
-
 const todos = ['Comprar manzanas', 'Pasear al gato']
 
-function clickHandler(event) {
+
+function submitHandler(event) {
+    // Con esto evitamos que el form haga reload en la página (Qué es el comportamiento por defecto)
     event.preventDefault()
+    // Guardamos el texto de nuestro form
     const todoText= todoInput.value
     if(todoText === ''){
         alert('Por favor ingresa una tarea')
         return
     }
-    
+    // Agregamos la tarea al Array de todo's
     todos.push(todoText)
     renderTodos()
+
+    // Limpiar el input
+    todoInput.value = ''
 }
 
+// Con esta función hacemos el render en nuestro sitio
 function renderTodos(){
     todosContainer.innerHTML = '';
     for (let i = 0; i < todos.length; i++) {
@@ -27,6 +34,8 @@ function renderTodos(){
     }
 }
 
-addButton.addEventListener('click', clickHandler )
+// addButton.addEventListener('click', submitHandler )
+form.addEventListener('submit',submitHandler)
+
 
 renderTodos()
