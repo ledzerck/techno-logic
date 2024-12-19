68ab316ce882101ffa5fe48e1c4d0b74ee78398d
Aldo <aldomapler@gmail.com>
2024-12-18 19:11:47 -0600

    improvement: Mejora en la UI

--- 00-DOM/scripts/app.js
+++ 00-DOM/scripts/app.js
@@ -5,8 +5,9 @@ const form = document.querySelector('#create-to-do-form')
 
 // Variable global para guardar los ToDo's
 let todos = [
-   {id: 1734543653127, label: "Comprar manzanas"},
-   {id: 1734543658321, label: "Pasear al perro"}
+   {id: 1734543653127, label: "Comprar manzanas", status: "Activo"},
+   {id: 1734543658321, label: "Pasear al perro", status: "Pendiente"},
+   {id: 1734543659506, label: "Estudiar para el examen", status: "Terminado"}
 ]
 
 
@@ -41,7 +42,7 @@ function submitHandler(event) {
 // Esta función borra las tareas del render
 function deleteHandler(event) {
     // Seleccionamos el elemento padre del botón de borrar que se presionó y obtenemos su id, que contiene un Date.now
-    const idToDelete = event.target.parentNode.getAttribute('data-id')
+    const idToDelete = event.target.parentNode.parentNode.getAttribute('data-id')
 
     const newTodos = []
 
@@ -64,21 +65,30 @@ function renderTodos(){
     for (let i = 0; i < todos.length; i++) {
         // Creamos el contenedor de cada tarea
         const todoItem = document.createElement('article')
+        todoItem.setAttribute('class', 'todo')
         todoItem.setAttribute('data-id', todos[i].id)
 
         // Creamos un span, que contenga la tarea
         const todoContent = document.createElement('span')
+        todoContent.setAttribute('class', 'todo-name')
         todoContent.textContent = todos[i].label
+
+        // Creamos un span que contenga el status
+        const todoStatus = document.createElement('span')
+        todoStatus.setAttribute('class', `todo-status ${todos[i].status}`)
+        todoStatus.textContent = todos[i].status
 
         // Creamos un botón para borrar el elemento
         const button = document.createElement('button')
-        button.textContent = 'Borrar'
+        button.setAttribute('class','action eliminar')
+        button.innerHTML = '<i class="bi bi-trash3-fill"></i>'
 
         // Agregamos un EventListener para borrar
         button.addEventListener('click', deleteHandler)
 
         // Insertamos la tarea y el botóno de borrar en cada contenedor de tarea
         todoItem.appendChild(todoContent)
+        todoItem.appendChild(todoStatus)
         todoItem.appendChild(button)
 
         // Insertamos la tarea en el contenedor de Tareas
